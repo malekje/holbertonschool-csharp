e820fff4eca3f90d0e6e5d18f9daa62a6931b629
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-03-24 12:24:22 +0000

    script

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+
 class Program
 {
 	static void Main(string[] args)
 	{
 		int number = 972;
-		Console.WriteLine(number + " Mission Street");
-        Console.WriteLine();
+		Console.WriteLine("{0} Mission Street", number);
 	}
 }
