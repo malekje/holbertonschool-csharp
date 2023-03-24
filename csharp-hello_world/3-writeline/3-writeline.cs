5b11b0c908ce26a404f5a2eaa255b8876f212715
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-03-24 12:07:31 +0000

    cs

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class program {
+class Program {
     static void Main(string[] args){
         Console.WriteLine("Programming is like building a multilingual puzzle");
         Console.WriteLine();
