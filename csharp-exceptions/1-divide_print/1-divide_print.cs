3485cbfe417149dbcc1df35804f21b59809b8c77
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-04-13 13:44:08 +0000

    int div

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,19 @@
+﻿class Int
+{
+    public static void divide(int a, int b)
+    {
+        int z = 0;
+        try
+        {
+            z = (a/b);
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, z);
+        }
+    }
+}
