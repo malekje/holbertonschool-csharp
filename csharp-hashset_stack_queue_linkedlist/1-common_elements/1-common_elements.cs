ad73ae6ca361969f641db42cd7e25c0834fffd46
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-05-17 12:23:21 +0000

    CSharp LIFO FIFO

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> nList = new List<int>();
+        foreach (var item in list1)
+        {
+            if (list2.Contains(item))
+                nList.Add(item);
+        }
+        return nList;
+    }
+}
