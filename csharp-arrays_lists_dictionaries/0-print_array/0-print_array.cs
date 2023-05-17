5d1d6bee3e05e21174ee07566dc3c2bcece7ed32
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-05-17 12:16:54 +0000

    CSharp arrays

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int [] narray = new int [size];
+        for (int i = 0; i < narray.Length; i++)
+        {
+            narray[i] = i;
+            Console.Write(narray[i]);
+            if (i != size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return narray;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+       List<int> iList = new List<int>();
+       for (int i = 0; i < size; i++)
+       {
+           iList.Add(i);
+           Console.Write(iList[i]);
+           if (i != size - 1)
+            Console.Write(" ");
+       }
+       Console.WriteLine();
+       return iList;
+    }
+}
