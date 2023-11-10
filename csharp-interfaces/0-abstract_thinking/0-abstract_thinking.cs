7f06dfe20cad74b93f86c6a8e9cfe47884d3b350
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2023-11-10 11:51:55 +0000

    abstract fix comment

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -3,8 +3,10 @@
 /// Abstract Base class
 public abstract class Base
 {
+    /// Name
     public string name;
 
+    /// Override method
     public override string ToString()
     {
         return (name + " is a " + this.GetType());
